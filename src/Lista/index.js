e71cd6039a3c666276cdf07d4949e2a7f717acff
Leonice <leonice@pop-os.localdomain>
2022-03-04 10:26:32 -0300

    Modificações em img.like

--- src/Lista/index.js
+++ src/Lista/index.js
@@ -8,6 +8,50 @@ class Lista extends Component{
         this.state={
          feed: this.props.data
         };
+        this.carregaIcone = this.carregaIcone.bind(this);
+        this.mostraLikes = this.mostraLikes.bind(this);
+        this.like = this.like.bind(this);
+    }
+    carregaIcone(likeada){
+        return likeada ? require('../img/likeada.png'):
+        require('../img/like.png')
+    }
+    like(){
+        let feed = this.state.feed;
+        
+        if(feed.likeada === true){
+            this.setState({
+                feed: {
+                    ...feed,
+                    likeada: false,
+                    likers: feed.likers - 1
+                }
+            });
+
+        }else{
+            this.setState({
+                feed: {
+                    ...feed,
+                    likeada: true,
+                    likers: feed.likers + 1
+                }
+            });
+        }
+    }
+
+    mostraLikes(likers){
+        let feed = this.state.feed;
+        
+        if(feed.likers <= 0){
+            return;
+        }
+
+        return(
+            <Text style = {styles.likes}>
+                {feed.likers} {feed.likers > 1 ? 'curtidas' : 'curtida'}
+            </Text>
+        );
+        
     }
     render(){
         return(
@@ -29,9 +73,9 @@ class Lista extends Component{
                 />
 
                 <View style={styles.areaBtn}>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={this.like}>
                         <Image
-                        source={require('../img/like.png')}
+                        source={this.carregaIcone(this.state.feed.likeada)}
                         style={styles.iconelike}
                         />
                     </TouchableOpacity>
@@ -42,6 +86,7 @@ class Lista extends Component{
                         />
                     </TouchableOpacity>
                 </View>
+                {this.mostraLikes(this.state.feed.likers)}
 
                 <View style={styles.viewRodape}>
                     <Text style={styles.nomeRodape}>
@@ -106,6 +151,10 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#000',
         paddingLeft: 5,
+    },
+    likes: {
+        fontWeight: 'bold',
+        marginLeft: 5,
     }
 });
 

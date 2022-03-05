afb2c5ee64827006f96a37dd4a7c5bf0b13e4608
Leonice <leonice@pop-os.localdomain>
2022-03-05 16:57:41 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,115 +1,96 @@
 import React, { Component} from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity, FlatList } from 'react-native';
-
-import Lista from './src/Lista';
+import { Text, View, StyleSheet, Image,TextInput, TouchableOpacity, FlatList, } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
 
 class App extends Component{
-
-  constructor(props){
-    super(props);
-    this.state = {
-      feed: [
-        {
-          id: '1',
-          nome: 'Lucas Silva',
-          descricao: 'Mais um dia de muitos bugs :)', 
-          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png',
-          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',
-          likeada: false, 
-          likers: 1
-        },
-        {
-          id: '2', 
-          nome: 'Matheus', 
-          descricao: 'Isso sim é ser raiz!!!!!', 
-          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
-          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
-          likeada: false, 
-          likers: 0
-        },
-        {
-          id: '3', 
-          nome: 'Jose Augusto', 
-          descricao: 'Bora trabalhar Haha', 
-          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
-          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
-          likeada: false, 
-          likers: 3
-        },
-        {
-          id: '4', 
-          nome: 'Gustavo Henrique', 
-          descricao: 'Isso sim que é TI!', 
-          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
-          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
-          likeada: false, 
-          likers: 1
-        },
-        {
-          id: '5', 
-          nome: 'Guilherme', 
-          descricao: 'Boa tarde galera do insta...', 
-          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
-          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
-          likeada: false, 
-          likers: 32
-        }
-      ]
-    };
-  }
   render(){
     return(
     <View style={styles.container}>
-
-      <View style={styles.header}>
-        <TouchableOpacity>
-        <Image
-        source={require('./src/img/logo.png')}
-        style={styles.logo}
-        />
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-        <Image
-        source={require('./src/img/send.png')}
-        style={styles.send}
-        />
-        </TouchableOpacity>
-      
-      </View>
-
-      <FlatList
-      showsVerticalScrollIndicator={false}
-      keyExtractor={(item) => item.id}
-      data={this.state.feed}
-      renderItem={ ({item}) => <Lista data={item}/>}
+      <LinearGradient
+        // Background Linear Gradient
+        colors={['#ffe4c4', '#e9967a']}
+        style={styles.background}
       />
 
-     
+      <View style={styles.content}>
+        
+        <Image 
+        source={require('./src/img/DNS-3.png')}
+        style={styles.logo}
+        />
+
+        <Text style={styles.texto}>
+          Leonice Ruvienski
+          Desenvolvedora de Software
+        </Text>
+
+        <TouchableOpacity style={styles.btn}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnTexto}>Mais Informações</Text>
+          </View>
+        </TouchableOpacity>
+
+        
+    
+
+      </View>
+         
     </View>
     );
   }
 }
 const styles = StyleSheet.create({
   container:{
-    flex:1,
+    backgroundColor: '#e9967a',
+    flex:1, 
+    alignItems: 'center',    
   },
-  header:{
-    height: 55,
-    backgroundColor: '#FFF',
-    flexDirection: 'row',
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height:550
+  },
+  content:{
     alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: 5,
-    borderBottomWidth: 0.2,
-    shadowColor: '#000',
-    elevation: 1,
-    marginTop: 40
+    justifyContent: 'center'
+  
   },
-  send:{
-    width: 23,
-    height: 23,
+  logo: {
+    marginTop: 100,
+    height: 150,
+    width: 150, 
+       
   },
+  texto:{   
+    padding: 10,
+    color: '#fff',
+    fontSize: 25,
+    textAlign: 'center',
+    margin: 20,
+    fontWeight: 'bold',
+  },
+  btn:{
+    width: 120,
+    height: 25,
+    borderWidth: 0,    
+    borderRadius: 25,
+    backgroundColor: '#ff5555ff'
+    
+  },
+  btnArea:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto:{
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: '#fff'
+
+  }
  
 });
 
